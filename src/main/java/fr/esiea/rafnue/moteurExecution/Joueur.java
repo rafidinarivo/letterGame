2b1b568c21879beb8f54eb2cfb4da3088f3d5369
lti <lti@mail.fr>
2017-02-23 16:27:34 +0100

    ajout joueur

---
+++ src/main/java/fr/esiea/rafnue/moteurExecution/Joueur.java
@@ -0,0 +1,41 @@
+package fr.esiea.rafnue.moteurExecution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * Represente un joueur
+ */
+public class Joueur {
+	
+	private String nom;
+	private List<String> motsValidseTrouves;
+	private List<Character> lettreUtilisateur;
+	
+	public Joueur(String nom) {
+		this.nom = nom;
+		this.motsValidseTrouves = new ArrayList<>();
+		this.lettreUtilisateur = new ArrayList<>();
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public void ajouterMot(String mot) {
+		this.motsValidseTrouves.add(mot);
+	}
+	
+	public void ajouterLettreUtilisateur(Character lettre) {
+		this.lettreUtilisateur.add(lettre);
+	}
+	
+	public void retirerMot(String mot) {
+		this.motsValidseTrouves.remove(mot);
+	}
+}
