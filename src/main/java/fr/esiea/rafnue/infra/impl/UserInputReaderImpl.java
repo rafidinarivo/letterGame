088c11545b474c4b1f23f1aa9b66ba10de0c9963
lti <lti@mail.fr>
2017-02-23 13:27:59 +0100

    Debut implementation interface de lecture des entrées

---
+++ src/main/java/fr/esiea/rafnue/infra/impl/UserInputReaderImpl.java
@@ -0,0 +1,70 @@
+package fr.esiea.rafnue.infra.impl;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import fr.esiea.rafnue.infra.UserInputReader;
+
+/**
+ * Implementation par defaut
+ * 
+ * @author lti
+ *
+ */
+public class UserInputReaderImpl implements UserInputReader {
+
+	public UserInputReaderImpl() {
+		super();
+	}
+
+	@Override
+	public String readSingleWord() {
+		Scanner scanner = null;
+		String result = null;
+		boolean finDeSaisie = false;
+
+		while (!finDeSaisie) {
+			try {
+				scanner = new Scanner(System.in);
+				System.out.println("Quel mot saissez-vous ? (Appuyer sur ENTREE pour ne rien saisir)");
+
+				boolean hasNext = scanner.hasNext();
+				
+				if (!hasNext) {
+					System.out.println("Vous ne souhaitez rien saisir, confirmez-vous ce choix ? (Appuyer sur ENTREE pour valider ou saisissez votre mot)"); 
+				}
+				
+				result = scanner.nextLine();
+			} catch (NoSuchElementException e) {
+				finDeSaisie = true;
+			} finally {
+				if (scanner != null) {
+					scanner.close();
+				}
+			}
+			
+			finDeSaisie = true;
+		}
+
+		return result;
+	}
+
+	@Override
+	public String[] readWords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Character readSingleLetter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Character[] readTwoLetter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
